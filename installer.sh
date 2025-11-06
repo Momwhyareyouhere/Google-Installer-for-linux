89f78c943a78a3c7a1834139c4850f121d2dee62
Momwhyareyouhere <151050626+Momwhyareyouhere@users.noreply.github.com>
2025-11-06 18:58:13 +0100

    Refactor installer.sh by removing checks
    Removed root and system checks from installer script.

--- installer.sh
+++ installer.sh
@@ -16,38 +16,8 @@ download_script() {
     fi
 }
 
-check_root() {
-    if [ "$(id -u)" -ne 0 ]; then
-        exit 1
-    fi
-}
 
-check_system() {
-    if ! grep -qE "(debian|ubuntu)" /etc/os-release; then
-        exit 1
-    fi
-}
 
-install_chrome() {
-    read -p "Do you want to proceed? (Y/N): " choice
-
-    if [[ "$choice" == "Y" || "$choice" == "y" ]]; then
-        sudo apt remove -y firefox
-        sudo rm -Rf /usr/bin/firefox
-        sudo rm -Rf /usr/local/firefox
-        sudo snap remove firefox
-
-        wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O google-chrome.deb
-        sudo dpkg -i google-chrome.deb
-        sudo apt --fix-broken install -y
-
-        rm google-chrome.deb
-    elif [[ "$choice" == "N" || "$choice" == "n" ]]; then
-        exit 0
-    else
-        exit 1
-    fi
-}
 
 download_script "https://raw.githubusercontent.com/Momwhyareyouhere/Google-Installer-for-linux/refs/heads/main/setup.sh" "setup.sh"
 download_script "https://raw.githubusercontent.com/Momwhyareyouhere/Google-Installer-for-linux/refs/heads/main/restore.sh" "restore.sh"
@@ -56,8 +26,7 @@ chmod +x "$DOWNLOAD_DIR/setup.sh" "$DOWNLOAD_DIR/restore.sh"
 clear
 sudo bash "$DOWNLOAD_DIR/setup.sh"
 
-check_root
-check_system
+
 
 
 rm -- "$0"
