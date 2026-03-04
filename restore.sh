6724661a3626658f1d53492c189b9c282b66a728
Momwhyareyouhere <151050626+Momwhyareyouhere@users.noreply.github.com>
2026-03-04 17:00:41 +0100

    Add support for Arch Linux in restore script

--- restore.sh
+++ restore.sh
@@ -5,12 +5,18 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-if ! grep -qE "(debian|ubuntu)" /etc/os-release; then
-    echo "This script is only supported on Debian or Ubuntu systems."
+if grep -qiE "(debian|ubuntu)" /etc/os-release; then
+    echo "Detected Debian/Ubuntu system."
+    apt update
+    apt install -y firefox
+
+elif grep -qi "arch" /etc/os-release; then
+    echo "Detected Arch Linux system."
+    pacman -Sy --noconfirm firefox
+
+else
+    echo "This script is only supported on Debian, Ubuntu, or Arch Linux systems."
     exit 1
 fi
-
-sudo apt update
-sudo apt install -y firefox
 
 echo "Done, Firefox has been restored."
